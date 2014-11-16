470cbaf085cb2fa045a000e4210bf75e269e0ef3
Luc <lucj@uw.edu>
2014-11-16 00:30:46 -0800

    put all images in an array and started css file

---
+++ js/images.js
@@ -0,0 +1,98 @@
+var images = [
+	{
+		"image" : "img/tile1.jpg"
+	},
+	{
+		"image" : "img/tile2.jpg"
+	},
+	{
+		"image" : "img/tile3.jpg"
+	},
+	{
+		"image" : "img/tile4.jpg"
+	},
+	{
+		"image" : "img/tile5.jpg"
+	},
+	{
+		"image" : "img/tile6.jpg"
+	},
+	{
+		"image" : "img/tile7.jpg"
+	},
+	{
+		"image" : "img/tile8.jpg"
+	},
+	{
+		"image" : "img/tile9.jpg"
+	},
+	{
+		"image" : "img/tile10.jpg"
+	},
+	{
+		"image" : "img/tile11.jpg"
+	},
+	{
+		"image" : "img/tile12.jpg"
+	},
+	{
+		"image" : "img/tile13.jpg"
+	},
+	{
+		"image" : "img/tile14.jpg"
+	},
+	{
+		"image" : "img/tile15.jpg"
+	},
+	{
+		"image" : "img/tile16.jpg"
+	},
+	{
+		"image" : "img/tile17.jpg"
+	},
+	{
+		"image" : "img/tile18.jpg"
+	},
+	{
+		"image" : "img/tile19.jpg"
+	},
+	{
+		"image" : "img/tile20.jpg"
+	},
+	{
+		"image" : "img/tile21.jpg"
+	},
+	{
+		"image" : "img/tile22.jpg"
+	},
+	{
+		"image" : "img/tile23.jpg"
+	},
+	{
+		"image" : "img/tile24.jpg"
+	},
+	{
+		"image" : "img/tile25.jpg"
+	},
+	{
+		"image" : "img/tile26.jpg"
+	},
+	{
+		"image" : "img/tile28.jpg"
+	},
+	{
+		"image" : "img/tile28.jpg"
+	},
+	{
+		"image" : "img/tile29.jpg"
+	},
+	{
+		"image" : "img/tile30.jpg"
+	},
+	{
+		"image" : "img/tile31.jpg"
+	},
+	{
+		"image" : "img/tile32.jpg"
+	}
+];
