14bfff8f97bee91d6ba7edca0949de5288585bf9
Luc <lucj@uw.edu>
2014-11-20 09:52:10 -0800

    changed some colors and sizing of tiles

--- js/app.js
+++ js/app.js
@@ -7,15 +7,32 @@ var fullSet = gameSet.concat(gameSet);
 var shuffledFullSet = _.shuffle(fullSet);
 
 // console.log(shuffledFullSet);
-
-for (var i = 0; i < shuffledFullSet.length; i++) {
-	var newTile = $(document.createElement('img'));
-	newTile.attr('src', 'img/tile-back.png')
-	var pic = shuffledFullSet[i]["image"];
-	newTile.data('frontImage', pic);
-	newTile.attr('width', '100px');
-	gameBoard.append(newTile);
+function populateRow(start, end) {
+	for (var i = start; i < end; i++) {
+		var newTile = $(document.createElement('img'));
+		var back = 'img/tile-back.png';
+		newTile.data('backImage', back);
+		newTile.attr('src', 'img/tile-back.png')
+		var front = shuffledFullSet[i]["image"];
+		newTile.data('frontImage', front);
+		// newTile.attr('width', '210px');
+		// newTile.attr('width', $('#game-board').width() / 4 - 30);
+		newTile.attr('height', $(document).height() / 4 - 30);
+		gameBoard.append(newTile);
+	}
 }
+
+// var gameWidth = $('#game-board').width();
+// console.log(gameWidth);
+// $('#game-board img').css("width", gameWidth);
+
+console.log(shuffledFullSet.length);
+console.log(shuffledFullSet);
+
+populateRow(0, 4);
+populateRow(4, 8);
+populateRow(8, 12);
+populateRow(12, 16);
 
 var clickedImages = 0;
 var firstImage;
@@ -47,15 +64,18 @@ $('#game-board img').click(function() {
 	if (firstImageData === secondImageData) {
 		console.log("match");
 		clickedImages = 0;
+		firstImage.addClass('matched');
+		secondImage.addClass('matched');
 	} else if (clickedImages == 2) {
 		console.log("no match");
 		setTimeout(function() {firstImage.attr('src', 'img/tile-back.png')}, 1000);
 		setTimeout(function() {secondImage.attr('src', 'img/tile-back.png')}, 1000);
+		firstImageData = firstImage.data('backImage');
+		secondImageData = secondImage.data('backImage');
 		clickedImages = 0;
 	}
-
 });
 
-$(onReady);
+// $(onReady);
 
 
